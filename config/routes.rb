c40e3366c7787b49d965cb1428cf195720f63da9
MHR-2477380 <ru.ru.ru.247738@gmail.com>
2020-04-26 14:49:35 +0000

    develop [edit] ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,33 @@
 Rails.application.routes.draw do
 
-  get 'bookmarks/create'
-  get 'bookmarks/destroy'
-root to: 'homes#top'
-get 'home/about' => 'homes#about'
+    root to: 'home#top'
+    get 'home/about' => 'homes#about'
 
-devise_for :users
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'bookmarks/create'
+    get 'bookmarks/destroy'
 
-resources :users
+    devise_for :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :articles do
-	resource  :favorites, only: [:create, :destroy]
-	resources :post_comments, only: [:create, :edit, :update, :destroy]
-	resource 	:bookmarks, only: [:create, :destroy]
+    resources :users
 
-	# ブックマークの一覧表示に使用するルーティング
-	get			:bookmarks, on: :collection
+    resources :articles do
+        resource  :favorites, only: [:create, :destroy]
+        resources :post_comments, only: [:create, :edit, :update, :destroy]
+        resource 	:bookmarks, only: [:create, :destroy]
 
-end
+        # ブックマークの一覧表示に使用するルーティング
+        get			:bookmarks, on: :collection
+    end
 
-resources :categories, only: [:index]
+    resources :categories, only: [:index]
 
-resources :questions do
-	resources :answers, only: [:create, :edit, :update, :destroy]
-	resource  :sympathizes, only: [:create, :destroy]
-end
+    resources :questions do
+        resources :answers, only: [:create, :edit, :update, :destroy]
+        resource  :sympathizes, only: [:create, :destroy]
+    end
 
-get 'search' => 'searches#search'
+    get 'search' => 'searches#search'
 
 
 end
